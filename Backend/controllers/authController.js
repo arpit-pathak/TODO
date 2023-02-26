27f977852822179810554312636e7683da655249
code-today <blogarpit21@gmail.com>
2023-02-26 23:29:12 +0530

    Add user login route and controller

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -57,3 +57,58 @@ exports.userRegister = async (req, res) => {
       .json({ success: false, message: "Error in response route" });
   }
 };
+
+// function for user Login
+exports.userLogIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if all the required fields are present in the request body
+    if (!(email && password)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are mandatory" });
+    }
+
+    // Find user with the given email and validate password
+    const user = await User.findOne({ email });
+    if (!(user && bcrypt.compare(password, user.password))) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email or Password is incorrect" });
+    }
+
+    // Generate token for the user
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      TOKEN_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    // Add token to the user object and remove password before sending response
+    user.token = token;
+    user.password = undefined;
+
+    // Set the token as a cookie and send response
+    res
+      .status(200)
+      .cookie("signIn", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 2 * 24 * 3600000),
+      })
+      .json({
+        success: true,
+        message: "Logged In Successfully",
+        token,
+        user,
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error in response route",
+    });
+  }
+};
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { userRegister } = require("../controllers/authController");
+const { userRegister, userLogIn } = require("../controllers/authController");
 
 router.post("/u/register", userRegister);
+router.post("/u/signIn", userLogIn);
 
 module.exports = router;
